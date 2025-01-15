5aff83023940d7e7f8688f0ea91ddba6520d792a
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-01-15 20:26:13 -0300

    :sparkles: feat: iniciando com vue

---
+++ masterclass_cod3r/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+
+const randomNumbers = ref(
+  Array.from({ length: 5 }, () => Math.floor(Math.random() * 6) + 1)
+);
+</script>
+
+<template>
+  <div v-for="number in randomNumbers" :key="number">
+    <HelloWorld :times="number" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ masterclass_cod3r/src/components/HelloWorld.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Meu Primeiro Componente Vue!</h1>
+</template>
