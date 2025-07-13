8c90b108f361856b97aad400914e0ed5b2bc6d29
Otavio Mendes Santos <otaviomendessantos2019@gmail.com>
2025-07-13 15:37:12 -0300

    :black_nib: course: Aula 34 - Estilizando Layout

--- curso_udemy/3-to_do_list/src/layouts/view.vue
+++ curso_udemy/3-to_do_list/src/layouts/view.vue
@@ -1,22 +1,28 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer">
-      <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/145459372?v=4" subtitle="To Do List"
-        title="Otávio M. S."></v-list-item>
-      <v-list :lines="false" density="compact" nav>
-        <router-link v-for="(item, i) in items" :key="i" :to="item.to">
-          <v-list-item :value="item" color="primary">
-            <template v-slot:prepend>
-              <v-icon :icon="item.icon"></v-icon>
-            </template>
-
-            <v-list-item-title :textContent="item.text"></v-list-item-title>
-          </v-list-item>
-        </router-link>
-      </v-list>
+      <v-img src="https://picsum.photos/1920/1080?random" gradient="to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)"
+        height="70px" cover class="pt-2">
+        <v-list :lines="false" density="compact" nav>
+          <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/145459372?v=4" subtitle="To Do List"
+            title="Otávio M. S."></v-list-item>
+        </v-list>
+      </v-img>
+      <router-link v-for="(item, i) in items" :key="i" :to="item.to">
+        <v-list-item :value="item" color="primary">
+          <template v-slot:prepend>
+            <v-icon :icon="item.icon"></v-icon>
+          </template>
+          <v-list-item-title :textContent="item.text"></v-list-item-title>
+        </v-list-item>
+      </router-link>
     </v-navigation-drawer>
 
-    <v-app-bar>
+    <v-app-bar image="https://picsum.photos/1920/1080?random" color="teal-darken-4" density="prominent">
+      <template v-slot:image>
+        <v-img gradient="to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)">
+        </v-img>
+      </template>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-app-bar-title>To do list</v-app-bar-title>
